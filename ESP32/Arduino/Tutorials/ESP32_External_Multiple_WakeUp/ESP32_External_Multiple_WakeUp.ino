37e48e4295ce396814567ab782749712945542fd
CoolDong <n_cooldong@naver.com>
2021-11-12 15:21:37 +0900

    .

--- ESP32/Arduino/Tutorials/ESP32_External_Multiple_WakeUp/ESP32_External_Multiple_WakeUp.ino
+++ ESP32/Arduino/Tutorials/ESP32_External_Multiple_WakeUp/ESP32_External_Multiple_WakeUp.ino
@@ -1,4 +1,4 @@
-#define BUTTON_PIN_BITMASK 0x8004
+#define BUTTON_PIN_BITMASK 0x8004 // 15번 2번 핀     16개(12~15)/12개(8~11)/8개(4~7)/4개(0~3) -> 2^3/0/0/2^2 (1=2^0, 2, 4, 8=2^3)
 
 RTC_DATA_ATTR int bootCount = 0;
 
