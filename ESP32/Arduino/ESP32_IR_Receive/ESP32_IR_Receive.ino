0a8171a388d884dae9bc9f6a76d0e213c763eb2e
lcooldong <n_cooldong@naver.com>
2021-12-01 19:59:51 +0900

    .

--- ESP32/Arduino/ESP32_IR_Receive/ESP32_IR_Receive.ino
+++ ESP32/Arduino/ESP32_IR_Receive/ESP32_IR_Receive.ino
@@ -15,17 +15,38 @@
  * Specify which protocol(s) should be used for decoding.
  * If no protocol is defined, all protocols are active.
  */
-#define DECODE_NEC          1
+ 
+/*
+#define DECODE_DENON        // Includes Sharp
+#define DECODE_JVC
+#define DECODE_KASEIKYO
+#define DECODE_PANASONIC    // the same as DECODE_KASEIKYO
+#define DECODE_LG
+#define DECODE_NEC          // Includes Apple and Onkyo
+#define DECODE_SAMSUNG
+#define DECODE_SONY
+#define DECODE_RC5
+#define DECODE_RC6
 
+#define DECODE_BOSEWAVE
+#define DECODE_LEGO_PF
+#define DECODE_MAGIQUEST
+#define DECODE_WHYNTER
+*/
+
+#define DECODE_NEC 1  // 디코딩 방식
 #include <IRremote.h>
 
-int IR_RECEIVE_PIN = 23;
+int IR_RECEIVE_PIN = 23;  // 데이터 핀
+uint32_t myRawdata= IrReceiver.decodedIRData.decodedRawData;
+
 
 void setup() {
     Serial.begin(115200);
     // Just to know which program is running on my Arduino
     Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
 
+    // 데이터 핀, 수신 확인용 LED<- 잘모르겠음
     IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver, enable feedback LED, take LED feedback pin from the internal boards definition
 
     Serial.print(F("Ready to receive IR signals at pin "));
@@ -37,10 +58,15 @@ void loop() {
      * Check if received data is available and if yes, try to decode it.
      * Decoded result is in the IrReceiver.decodedIRData structure.
      */
+     // 디코딩 된 정보 받으면
     if (IrReceiver.decode()) {
 
         // Print a short summary of received data
-        IrReceiver.printIRResultShort(&Serial);
+        IrReceiver.printIRResultShort(&Serial); // 받은 데이터 시리얼에 표시
+        Serial.println();
+        Serial.print("RawData : ");
+        Serial.println(myRawdata);
+        // 알 수 없는 것이 들어오면 추가 정보 출력
         if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
             // We have an unknown protocol, print more info
             IrReceiver.printIRResultRawFormatted(&Serial, true);
