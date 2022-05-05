816697e44ec9b2f7c6742789006e2920c5d1ee1c
CoolDong <n_cooldong@naver.com>
2022-05-05 19:49:03 +0900

    .

---
+++ ESP32/Keypad/4x3_keypad/4x3_keypad.ino
@@ -0,0 +1,30 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3}; //connect to the column pinouts of the keypad
+
+//Create an object of keypad
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(115200);
+}
+  
+void loop(){
+  char key = keypad.getKey();// Read the key
+  
+  // Print if key pressed
+  if (key){
+    Serial.print("Key Pressed : ");
+    Serial.println(key);
+  }
+}
