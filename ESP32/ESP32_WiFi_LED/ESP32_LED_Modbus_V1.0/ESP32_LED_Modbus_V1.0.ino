641b9c2072ea871db28fd7d123a8f0fd5d8f1395
CoolDong <n_cooldong@naver.com>
2022-05-14 02:29:04 +0900

    add modbus library

---
+++ ESP32/ESP32_WiFi_LED/ESP32_LED_Modbus_V1.0/ESP32_LED_Modbus_V1.0.ino
@@ -0,0 +1,172 @@
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+#include "OTA.h"
+#include <Adafruit_NeoPixel.h>
+#include <ModbusIP_ESP8266.h>
+#define ESP32_RTOS
+#define LED_PIN    23
+#define LED_COUNT 1
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+ModbusIP mb;  //ModbusIP object
+
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
+int connection_flag = 0;
+int cnt = 0;
+
+const int REG = 528;               // Modbus Hreg Offset
+IPAddress remote(192, 168, 30, 13);  // Address of Modbus Slave device
+uint16_t res = 0;
+
+
+void setup() {
+  Serial.begin(115200);
+  EEPROM.begin(EEPROM_SIZE);
+  setupOTA("ESP_LED");
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
+
+  mb.client();
+  initNeopixel(50);
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  if (mb.isConnected(remote)) {   // Check if connection to Modbus Slave is established
+    mb.readHreg(remote, REG, &res);  // Initiate Read Coil from Modbus Slave
+  } else {
+    mb.connect(remote);           // Try to connect if no connection
+  }
+  mb.task();                      // Common local Modbus task
+  delay(1);         
+  
+  //pickOneLED(0, strip.Color(255,   0,   0), 50);
+  rainbow(10);
+  TelnetStream.println(cnt);
+  cnt++;
+
+
+
+
+
+  
+}
+
+
+
+
+
+
+
+void reconnectWiFi(){
+  unsigned long currentMillis = millis();
+  // if WiFi is down, try reconnecting every CHECK_WIFI_TIME seconds
+  
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >=interval)) {
+    Serial.println(millis());
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+    connection_flag = 1;
+    previousMillis = currentMillis;
+  }
+  else if(connection_flag == 1)
+  {
+    Serial.println("reconnected");  
+    connection_flag = 0;
+  }
+}
+
+void initNeopixel(uint8_t brightness){
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(brightness); // Set BRIGHTNESS to about 1/5 (max = 255)
+}
+
+
+void pickOneLED(uint8_t ledNum, uint32_t color, int wait){
+    strip.setPixelColor(ledNum, color);         
+    strip.show();                                               
+    delay(wait);
+}
+
+
+// Some functions of our own for creating animated effects -----------------
+
+// Fill strip pixels one after another with a color. Strip is NOT cleared
+// first; anything there will be covered pixel by pixel. Pass in color
+// (as a single 'packed' 32-bit value, which you can get by calling
+// strip.Color(red, green, blue) as shown in the loop() function above),
+// and a delay time (in milliseconds) between pixels.
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+}
+
+// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
+// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
+// between frames.
+void theaterChase(uint32_t color, int wait) {
+  for(int a=0; a<10; a++) {  // Repeat 10 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in steps of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show(); // Update strip with new contents
+      delay(wait);  // Pause for a moment
+    }
+  }
+}
+
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    // strip.rainbow() can take a single argument (first pixel hue) or
+    // optionally a few extras: number of rainbow repetitions (default 1),
+    // saturation and value (brightness) (both 0-255, similar to the
+    // ColorHSV() function, default 255), and a true/false flag for whether
+    // to apply gamma correction to provide 'truer' colors (default true).
+    strip.rainbow(firstPixelHue);
+    // Above line is equivalent to:
+    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
+
+// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
+void theaterChaseRainbow(int wait) {
+  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
+  for(int a=0; a<30; a++) {  // Repeat 30 times...
+    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
+      strip.clear();         //   Set all pixels in RAM to 0 (off)
+      // 'c' counts up from 'b' to end of strip in increments of 3...
+      for(int c=b; c<strip.numPixels(); c += 3) {
+        // hue of pixel 'c' is offset by an amount to make one full
+        // revolution of the color wheel (range 65536) along the length
+        // of the strip (strip.numPixels() steps):
+        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
+        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+      }
+      strip.show();                // Update strip with new contents
+      delay(wait);                 // Pause for a moment
+      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
+    }
+  }
+}
