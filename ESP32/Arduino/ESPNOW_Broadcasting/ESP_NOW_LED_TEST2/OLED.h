6c29008f0861515a3e5031aae2c140ef26b276a6
CoolDong <n_cooldong@naver.com>
2022-05-20 21:54:12 +0900

    .

---
+++ ESP32/Arduino/ESPNOW_Broadcasting/ESP_NOW_LED_TEST2/OLED.h
@@ -0,0 +1,14 @@
+#ifndef OLED_h
+#define OLED_h
+
+#include <SPI.h>
+#include <Wire.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+void initOLED();
+void showOLED_IP_Address();
+void showOLED_WiFi(char* ssid, char* pass);
+void showOLED_changing_WiFi();
+#endif
