b6e5ca7722ef8b9612ec37da5dd0faea77716aec
CoolDong <n_cooldong@naver.com>
2024-02-16 11:15:56 +0900

    .

--- VScode/ESP32C3/ESP32C3_AS5048A/src/main.cpp
+++ VScode/ESP32C3/ESP32C3_AS5048A/src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
+#include <SimpleFOC.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int pwmMin = 4;
+const int pwmMax = 904;
+const uint8_t encorderPin = 7;
 
+//  PinPWM / min / max
+MagneticSensorPWM sensor1 = MagneticSensorPWM(encorderPin, pwmMin, pwmMax);
+
+
+uint64_t pwmLastTime = 0;
+void doPWM1(){sensor1.handlePWM();}
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  sensor1.init();
+  sensor1.enableInterrupt(doPWM1);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // if(millis() - pwmLastTime > 100)
+  // {
+  //   Serial.printf("Value : %f\r\n" , pulseIn(encorderPin, HIGH));
+  //   pwmLastTime = millis();
+  // }
+
+  sensor1.update();
+  Serial.print(sensor1.getAngle());
+  Serial.println();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
