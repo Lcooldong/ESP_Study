c1061e9515fbd1467274ff4732f364a710c2555c
CoolDong <n_cooldong@naver.com>
2025-07-01 17:58:23 +0900

    ESP32S3 Upload Test

--- VScode/ESP32S3_XIAO/XIAO_S3_Upload/src/main.cpp
+++ VScode/ESP32S3_XIAO/XIAO_S3_Upload/src/main.cpp
@@ -1,4 +1,80 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+#include "AsyncTCP.h"
+#include "ESPAsyncWebServer.h"
+#include "ElegantOTA.h"
+
+
+const char* ssid = "ESP32_S3_XIAO";
+const char* password = "1234567890";
+
+unsigned long ota_progress_millis = 0;
+
+AsyncWebServer server(80);
+
+void onOTAStart();
+void onOTAProgress(size_t current, size_t final);
+void onOTAEnd(bool success);
+
+
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    h2 {font-size: 3.0rem;}
+    p {font-size: 3.0rem;}
+    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+    .switch input {display: none}
+    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
+    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
+    input:checked+.slider {background-color: #b30000}
+    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+  </style>
+</head>
+<body>
+  <h2>ESP Web Server</h2>
+  %BUTTONPLACEHOLDER%
+<script>function toggleCheckbox(element) {
+  var xhr = new XMLHttpRequest();
+  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
+  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
+  xhr.send();
+}
+</script>
+</body>
+</html>
+)rawliteral";
+
+String outputState(int output){
+  if(digitalRead(output)){
+    return "checked";
+  }
+  else {
+    return "";
+  }
+}
+
+
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "BUTTONPLACEHOLDER"){
+    String buttons = "";
+    buttons += "<h4>Output - GPIO 2</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"2\" " + outputState(2) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Output - GPIO 4</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"4\" " + outputState(4) + "><span class=\"slider\"></span></label>";
+    buttons += "<h4>Output - GPIO 33</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"33\" " + outputState(33) + "><span class=\"slider\"></span></label>";
+    return buttons;
+  }
+  return String();
+}
+
+
 
 uint32_t lastMilis = 0;
 uint32_t currentMillis = 0;
@@ -9,6 +85,32 @@ void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT); // Initialize the built-in LED pin
   Serial.println("Starting the counter...");
+  
+  WiFi.mode(WIFI_AP); // Set WiFi mode to Access Point
+  WiFi.softAP(ssid, password); // Start the WiFi access point
+  delay(500);
+
+  IPAddress myIP = WiFi.softAPIP(); // Get the IP address of the access point
+  Serial.print("Access Point IP address: ");
+  Serial.println(myIP); // Print the IP address
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", index_html, processor);
+  });
+
+  
+  // server.on("/ESP_UPDATE", HTTP_GET, [](AsyncWebServerRequest *request) {
+  //   request->send(200, "text/plain", "Hi! This is ElegantOTA AsyncDemo.");
+  // });
+
+  ElegantOTA.begin(&server);    // Start ElegantOTA
+  // ElegantOTA callbacks
+  ElegantOTA.onStart(onOTAStart);
+  ElegantOTA.onProgress(onOTAProgress);
+  ElegantOTA.onEnd(onOTAEnd);
+
+
+  server.begin();
 }
 
 void loop() {
@@ -21,5 +123,32 @@ void loop() {
     ledState = !ledState; // Toggle the LED state
     digitalWrite(LED_BUILTIN, ledState ? HIGH : LOW); // Update the
   }
+  
+  ElegantOTA.loop();
 }
 
+
+
+void onOTAStart() {
+  // Log when OTA has started
+  Serial.println("OTA update started!");
+  // <Add your own code here>
+}
+
+void onOTAProgress(size_t current, size_t final) {
+  // Log every 1 second
+  if (millis() - ota_progress_millis > 1000) {
+    ota_progress_millis = millis();
+    Serial.printf("OTA Progress Current: %u bytes, Final: %u bytes\n", current, final);
+  }
+}
+
+void onOTAEnd(bool success) {
+  // Log when OTA has finished
+  if (success) {
+    Serial.println("OTA update finished successfully!");
+  } else {
+    Serial.println("There was an error during OTA update!");
+  }
+  // <Add your own code here>
+}
