e77d7712a549bf6eb062dfe3577ae582c25b8a68
CoolDong <n_cooldong@naver.com>
2025-07-23 21:59:42 +0900

    .

--- VS/CPP_HID/CPP_HID/main.cpp
+++ VS/CPP_HID/CPP_HID/main.cpp
@@ -1,62 +1,90 @@
 #include <iostream>
+#include <stdexcept>
 #include <SDL3/SDL.h>
 
+
+
 //#pragma comment(lib, "../x64/Debug/lib/x64/SDL3.lib")
+
+class SDLExeption final : public std::runtime_error
+{
+public:
+    explicit SDLExeption(const std::string& message) : std::runtime_error(message + ": " + SDL_GetError()) {}
+ 
+};
+
 
 int main()
 {
     std::cout << "HID"<< std::endl;
+	SDL_Window* window = NULL;
 
-    if (SDL_Init(SDL_INIT_GAMEPAD) < 0) {
-        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
-        return 1;
+    if (SDL_Init(SDL_INIT_GAMEPAD | SDL_INIT_JOYSTICK | SDL_INIT_VIDEO) < 0) {
+        SDL_Log("SDL_Init failed: %s\n", SDL_GetError());
+        std::cout << "SDL could not be initialized: " << SDL_GetError(); 
+    }
+    else 
+    {
+        std::cout << "SDL video system and joystick is ready to go\r\n ";
     }
 
-    int num_joysticks = SDL_HasGamepad();
-    std::cout << "Joysticks/Gamepads detected: " << num_joysticks << std::endl;
+	window = SDL_CreateWindow("SDL Gamepad Example", 640, 480, SDL_WINDOW_HIDDEN | SDL_WINDOW_RESIZABLE);
+    if (!window)
+        throw SDLExeption("Couldn't create window");
 
-    SDL_Gamepad* gamepad = nullptr;
+    SDL_GPUDevice* gpuDevice{
 
-    for (int i = 0; i < num_joysticks; ++i) {
-        if (SDL_IsGamepad(i)) {
-            gamepad = SDL_OpenGamepad(i);
-            if (gamepad) {
-                std::cout << "Opened Gamepad: " << SDL_GetGamepads(&num_joysticks) << std::endl;
-                break;
-            }
-        }
-    }
+        SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV | SDL_GPU_SHADERFORMAT_MSL | SDL_GPU_SHADERFORMAT_DXIL, true, nullptr)
+    };
 
-    if (!gamepad) {
-        std::cerr << "No gamepad could be opened.\n";
-        SDL_Quit();
-        return 1;
-    }
+    if (!gpuDevice) 
+		throw SDLExeption("Couldn't create GPU device");
 
-    SDL_Event event;
-    bool running = true;
 
-    while (running) {
+    if (!SDL_ClaimWindowForGPUDevice(gpuDevice, window)) 
+		throw SDLExeption("Couldn't claim window for GPU device");
+
+    SDL_ShowWindow(window);
+
+    bool isRunning{ true };
+	SDL_Event event;
+
+    while (isRunning)
+    {
         while (SDL_PollEvent(&event)) {
-            switch (event.type) {
+            switch (event.type)
+            {
             case SDL_EVENT_QUIT:
-                running = false;
-                break;
-            case SDL_EVENT_GAMEPAD_BUTTON_DOWN:
-                std::cout << "[BUTTON DOWN] Button: " << SDL_GetGamepadStringForButton(
-                    static_cast<SDL_GamepadButton>(event.gbutton.button)) << std::endl;
-                break;
-            case SDL_EVENT_GAMEPAD_AXIS_MOTION:
-                std::cout << "[AXIS] Axis: " << SDL_GetGamepadStringForAxis(
-                    static_cast<SDL_GamepadAxis>(event.gaxis.axis)) << " ¡æ " << event.gaxis.value << std::endl;
+                isRunning = false;
+			    //break;
+
+            default:
                 break;
             }
         }
-
-        SDL_Delay(10);
     }
 
-    SDL_CloseGamepad(gamepad);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, "NAME");
+
+    SDL_Rect rect;
+	rect.x = 50;
+	rect.y = 50;
+	rect.w = 200;
+	rect.h = 200;
+
+
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer); // Clear the renderer with the current draw color\
+
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	 
+	
+    
+	SDL_RenderPresent(renderer);
+
+  
+
+    
     SDL_Quit();
 
     return 0;
