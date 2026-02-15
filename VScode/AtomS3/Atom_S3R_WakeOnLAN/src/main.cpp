c3db8da79bcd5da524f8df3f641257509f28b5ed
CoolDong <s_cooldong@naver.com>
2026-02-15 13:37:14 +0900

    .

---
+++ VScode/AtomS3/Atom_S3R_WakeOnLAN/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <M5Unified.h>
+
+
+// #include <Ethernet.h>
+// #include <EthernetUdp.h>
+// #include <>
+
+#define SCK  22
+#define MISO 23
+#define MOSI 33
+#define CS   19
+
+
+
+
+void setup() {
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
